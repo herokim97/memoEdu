dbf997ec7ef6e040cd6fc94f5b05ba756da77637
heroKim997 <ghkufud6545@gmail.com>
2024-11-04 20:43:16 +0900

    Initial commit

---
+++ src/main/java/org/example/memoedu/MemoEduApplication.java
@@ -0,0 +1,13 @@
+package org.example.memoedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoEduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/memoedu/MemoEduApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.memoedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoEduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
